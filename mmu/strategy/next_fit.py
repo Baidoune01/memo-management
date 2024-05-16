2fc607421b68fc48577f7975708dc28b4bcefcb4
Baidoune01 <abderrahmane.baidoune@um6p.ma>
2024-05-16 14:53:51 +0100

    new repo

---
+++ mmu/strategy/next_fit.py
@@ -0,0 +1,98 @@
+import math
+from typing import List
+from mmu.models.memory_block import MemoryBlock
+from mmu.strategy.memory_allocation_strategy import MemoryAllocationStrategy
+
+class NextFitStrategy(MemoryAllocationStrategy):
+    """
+    A memory allocation strategy that allocates memory using the Next Fit algorithm.
+
+    Attributes:
+        next (int): The index to start searching for available memory in the next iteration.
+
+    Methods:
+        __init__(self):
+            Initializes a NextFitStrategy object.
+
+        allocate_memory(self, amount: int, memory_blocks: List[MemoryBlock]) -> int:
+            Allocates memory using the Next Fit algorithm.
+
+    """
+
+    def __init__(self):
+        """
+        Initializes a NextFitStrategy object.
+
+        Args:
+            None
+        """
+        self.next = 0
+
+    def allocate_memory(self, amount: int, memory_blocks: List[MemoryBlock]) -> int:
+        """
+        Allocates memory using the Next Fit algorithm.
+
+        Args:
+            amount (int): The amount of memory to allocate.
+            memory_blocks (List[MemoryBlock]): List of MemoryBlock objects representing available memory blocks.
+
+        Returns:
+            Tuple[int, int]: A tuple containing the index of the allocated memory block and the allocated memory size.
+                             If no suitable space is found for allocation, returns (-1, None).
+        """
+        available_space = ()
+        current_space = 0
+        block_size = memory_blocks[0].size
+
+        # Look for the first available space in memory starting from where we stopped in the previous iteration
+        for index, block in enumerate(memory_blocks[self.next : ]):
+            if block.is_free:
+                current_space += block.size
+            if not block.is_free or block == memory_blocks[-1]:
+                if block == memory_blocks[-1]:
+                    if current_space >= amount:
+                        available_space = (index + self.next + 1 - current_space // block_size, current_space)
+                    current_space = 0
+                else:
+                    if current_space >= amount:
+                        available_space = (index + self.next - current_space // block_size, current_space)
+                        break
+                    current_space = 0
+        
+        if not available_space:
+            current_space = 0
+            if self.next != 0:
+                # Start searching from the beginning of memory if no suitable space found from the current position
+                for index, block in enumerate(memory_blocks[ : ]):
+                    if block.is_free:
+                        current_space += block.size
+                    if not block.is_free or block == memory_blocks[-1]:
+                        if block == memory_blocks[-1]:
+                            if current_space >= amount:
+                                available_space = (index + 1 - current_space // block_size, current_space)
+                            current_space = 0
+                        else:
+                            if current_space >= amount:
+                                available_space = (index - current_space // block_size, current_space)
+                                break
+                            current_space = 0
+                if not available_space:
+                    return -1, None # No suitable space found for allocation
+            else:
+                return -1, None # No suitable space found for allocation
+
+        next_fit = available_space
+        print(next_fit)
+        allocated_mem_slots = math.ceil(amount / block_size)
+        # Update the starting index for the next iteration
+        if (next_fit[0] + allocated_mem_slots == len(memory_blocks)):
+            self.next = 0
+        else:
+            self.next = next_fit[0] + allocated_mem_slots
+        print(self.next)
+
+        # Mark allocated memory slots as occupied
+        for i in range(allocated_mem_slots):
+            memory_blocks[next_fit[0] + i].is_free = False
+
+        return next_fit[0], amount
---
+++ mmu/strategy/worst_fit.py
@@ -0,0 +1,59 @@
+import math
+from typing import List
+from mmu.models.memory_block import MemoryBlock
+from mmu.strategy.memory_allocation_strategy import MemoryAllocationStrategy
+
+class WorstFitStrategy(MemoryAllocationStrategy):
+    """
+    A memory allocation strategy that allocates memory using the Worst Fit algorithm.
+
+    Attributes:
+        None
+
+    Methods:
+        allocate_memory(self, amount: int, memory_blocks: List[MemoryBlock]) -> int:
+            Allocates memory using the Worst Fit algorithm.
+
+    """
+
+    def allocate_memory(self, amount: int, memory_blocks: List[MemoryBlock]) -> int:
+        """
+        Allocates memory using the Worst Fit algorithm.
+
+        Args:
+            amount (int): The amount of memory to allocate.
+            memory_blocks (List[MemoryBlock]): List of MemoryBlock objects representing available memory blocks.
+
+        Returns:
+            Tuple[int, int]: A tuple containing the index of the allocated memory block and the allocated memory size.
+                             If no suitable space is found for allocation, returns (-1, None).
+        """
+        available_spaces = []
+        current_space = 0
+        block_size = memory_blocks[0].size
+
+        # Look for available spaces in memory
+        for index, block in enumerate(memory_blocks):
+            if block.is_free:
+                current_space += block.size
+            if not block.is_free or block == memory_blocks[-1]:
+                if block == memory_blocks[-1]:
+                    if current_space >= amount:
+                        available_spaces.append((index + 1 - current_space // block_size, current_space))
+                    current_space = 0
+                else:
+                    if current_space >= amount:
+                        available_spaces.append((index - current_space // block_size, current_space))
+                    current_space = 0
+                    
+        if not available_spaces:
+            return -1, None # No suitable space found for allocation
+
+        worst_fit = max(available_spaces, key=lambda x: x[1])
+        allocated_mem_slots = math.ceil(amount / block_size)
+
+        # Mark allocated memory slots as occupied
+        for i in range(allocated_mem_slots):
+            memory_blocks[worst_fit[0] + i].is_free = False
+
+        return worst_fit[0], amount
